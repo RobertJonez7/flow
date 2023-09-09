7304f19db3a9d048a1f3ad10993fc3316db960ac
unknown <robertdjonez@gmail.com>
2023-09-08 22:35:30 -0600

    fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import response from "./test/test-data";
-import Sidebar from "./components/Sidebars";
+import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Button from "./components/Button";
-import Grid from "./components/Grids";
+import Grid from "./components/Grid";
 import { createColorPallete } from "./helpers/create-color-pallete";
 import { useState } from "react";
 
